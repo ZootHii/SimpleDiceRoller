e01de661378109ed86ff06806b806a9ef42d7160
Ahmet <ahmet-yildirim_5858@outlook.com>
2020-10-07 16:06:40 +0300

    Initial commit

---
+++ Assets/Scripts/Dice/Dice.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour {
+
+	public Rigidbody rb;
+	public Vector3 diceVelocity;
+	public static Dice instance;
+		
+
+	private void Awake()
+	{
+		instance = this;
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(diceVelocity!= null)
+        {
+			diceVelocity = rb.velocity;
+		}
+		//Input.GetTouch(0).tapCount>0
+		//Input.GetKeyDown(KeyCode.Space)
+		var fingerCount = 0;
+		foreach(var touch in Input.touches)
+        {
+			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+				fingerCount++;
+		}
+
+		if (fingerCount > 0) {
+
+			Debug.Log("AHMET");
+
+			DiceNumber.diceNumber = 0;
+			float dirX = Random.Range (0, 500);
+			float dirY = Random.Range (0, 500);
+			float dirZ = Random.Range (0, 500);
+			float posX = Random.Range (-1, 1);
+			float posZ = Random.Range (-1, 1);
+
+			transform.position = new Vector3 (posX, 7, posZ);
+			transform.rotation = Quaternion.identity;
+			rb.AddForce (transform.up * 100);
+			rb.AddTorque (dirX, dirY, dirZ);
+		}
+	}
+}
---
+++ Assets/Scripts/Dice/DiceTrigger.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class DiceTrigger : MonoBehaviour
+{
+
+    Vector3 diceVelocity;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Dice.instance.diceVelocity != null)
+        {
+            diceVelocity = Dice.instance.diceVelocity;
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (diceVelocity.x == 0f && diceVelocity.y == 0f && diceVelocity.z == 0f)
+        {
+            switch (col.gameObject.name)
+            {
+                case "Side1":
+                    DiceNumber.diceNumber = 6;
+                    break;
+                case "Side2":
+                    DiceNumber.diceNumber = 4;
+                    break;
+                case "Side3":
+                    DiceNumber.diceNumber = 5;
+                    break;
+                case "Side4":
+                    DiceNumber.diceNumber = 2;
+                    break;
+                case "Side5":
+                    DiceNumber.diceNumber = 3;
+                    break;
+                case "Side6":
+                    DiceNumber.diceNumber = 1;
+                    break;
+            }
+        }
+    }
+}
